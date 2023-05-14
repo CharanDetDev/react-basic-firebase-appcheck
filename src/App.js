7bcc3545e7806d846e16bd3dadf019c86a5e1ca8
CharanDetDev <charandachkhrathok@gmail.com>
2023-05-14 17:40:54 +0700

    Update UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import {
+  initializeAppCheck,
+  getToken,
+  ReCaptchaV3Provider,
+} from "firebase/app-check";
+import { initializeApp } from "firebase/app";
 
 function App() {
+
+  let appCheckTokenResponse;
+  const reCaptchaV3SiteKet = "6LdFbAkmAAAAAAvz0nbcL99F_l4wzeCRz6K4AttP"
+  // ********************************************************************************
+  // ******************* Change your firebase configuration here. *******************
+  const app = initializeApp({
+    // apiKey: "XXXXXXXX",
+    // authDomain: "XXXXXXXX",
+    // projectId: "XXXXXXXX",
+    // storageBucket: "XXXXXXXX",
+    // messagingSenderId: "XXXXXXXX",
+    // appId: "XXXXXXXX",
+    // measurementId: "XXXXXXXX",
+  });
+  // ********************************************************************************
+  // ********************************************************************************
+
+  const appCheck = initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(reCaptchaV3SiteKet),
+    isTokenAutoRefreshEnabled: true,
+  });
+
+  const callApiWithAppCheckForGetToken = async () => {
+    try {
+      appCheckTokenResponse = await getToken(appCheck);
+    } catch (err) {
+      console.log("Error getToken => ", err);
+      return;
+    }
+    
+    console.log("appCheckTokenResponse TOKEN :: => ",appCheckTokenResponse.token);
+  };
+
+  const verifyAppCheckToken = async () => {
+    const bearer = "Bearer "
+    const response = await fetch("http://localhost:3001/authorization/appcheck", {
+      method: "post",
+      headers: {
+        "Authorization": bearer.concat("", appCheckTokenResponse.token),
+        "Content-Type": "application/json",
+      },
+    });
+    const text = await response.json();
+    console.log("response Verify AppCheck Token from API => ", JSON.stringify(text, null, 4));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <h3 style={{ margin: 0, color: '#fe9000'}}>F12 open inspect for Display AppCheck Token in side Console.</h3> 
+        <br/> 
+        <h4 style={{ margin: 0 }}>Generate AppCheck Token</h4>
+        <button onClick={callApiWithAppCheckForGetToken}>
+        Generate AppCheckToken
+        </button>
+        <hr/>
+        <h4 style={{ margin: 0 }}>Verify AppCheck Token</h4>
+        <button onClick={verifyAppCheckToken}>
+          Verify AppCheckfToken
+        </button>
       </header>
     </div>
   );
